cae370f1e3370da137a34fd0f0782fc3b120dfdb
Kazuto Kusama <jacopen@gmail.com>
2022-01-31 09:22:38 +0000

    Migrate to TFCB

--- terraform.tf
+++ terraform.tf
@@ -1,7 +1,20 @@
+terraform {
+  backend "remote" {
+    hostname     = "app.terraform.io"
+    organization = "kusama"
+
+    workspaces {
+      name = "vsphere-cluster"
+    }
+  }
+  required_providers {
+    avi = {
+      source  = "hashicorp/vsphere"
+      version = "2.0.2"
+    }
+  }
+}
+
 provider "vsphere" {
   allow_unverified_ssl = true
-  # Use environment variables to configure the vsphere provider
-  # VSPHERE_USER
-  # VSPHERE_PASSWORD
-  # VSPHERE_SERVER
 }
